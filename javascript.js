cdb08cd0e6a7b1a15e8c87af96a4348538002c8c
Adam Caldwell <adamcaldwell15@gmail.com>
2024-11-04 16:22:55 +0000

    keyboard support

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 let num1 = null;
 let num2 = null;
 let operator = null;
+let num1HasDec = false;
+let num2HasDec = false;
 
 const add = function(num1, num2) {
     return num1 + num2;
@@ -14,22 +16,38 @@ const multiply = function(num1, num2) {
 const divide = function(num1, num2) {
     return num1 / num2;
 }
-const operate = function(num1, num2, operator) {
+const operate = function() {
+    const num1copy = parseInt(num1);
+    const num2copy = parseInt(num2);
+    console.log("num1copy: " + num1copy);
+    console.log("num2copy: " + num2copy);
+    const operatorcopy = operator;
+    let ans;
     clearDisplay();
-    if (num2 == 0 && operator == "/") {return "Don't do that."}
-    switch (operator) {
+    if (num2copy == 0 && operatorcopy == "/") {return "Don't do that."}
+    switch (operatorcopy) {
         case "+":
-            return (add(num1, num2).toFixed(2));
+            ans = add(num1copy, num2copy).toFixed(2);
+            if (ans.includes(".")) {num1HasDec = true;} else {num1HasDec = false;}
+            return ans;
         case "-":
-            return (subtract(num1, num2).toFixed(2));
+            ans = subtract(num1copy, num2copy).toFixed(2);
+            if (ans.includes(".")) {num1HasDec = true;} else {num1HasDec = false;}
+            return ans;
         case "*":
-            return (multiply(num1, num2).toFixed(2));
+            ans = multiply(num1copy, num2copy).toFixed(2);
+            if (ans.includes(".")) {num1HasDec = true;} else {num1HasDec = false;}
+            return ans;
         case "/":
-            return (divide(num1, num2).toFixed(2));
+            ans = divide(num1copy, num2copy).toFixed(2);
+            if (ans.includes(".")) {num1HasDec = true;} else {num1HasDec = false;}
+            return ans;
     }
 }
 let screencontent = document.querySelector("#screencontent");
 const clearDisplay = function () {
+    num1HasDec = false;
+    num2HasDec = false;
     screencontent.textContent = "";
     num1 = null;
     num2 = null;
@@ -40,34 +58,69 @@ const addToDisplay = function(string) {
     console.log("num1: " + num1);
     console.log("num2: " + num2);
     console.log("operator: " + operator);
+    console.log("num1HasDec: " + num1HasDec);
+    console.log("num2HasDec: " + num2HasDec);
     if (screencontent.textContent == "Don't do that.") {clearDisplay();}
+    //Divide by zero check
     if (string == "Don't do that.") {clearDisplay(); screencontent.textContent = string;} else {
-        if (parseInt(string) || string == "0") {
+        //If Number or decimal point
+        if (parseInt(string) || string == "0" || string == ".") {
+            //If nothing on screen
             if (num1 == null) {
-                num1 = parseInt(string);
-                screencontent.textContent = string;
+                if (string != ".") {
+                    num1 = string;
+                    screencontent.textContent = string;
+                }
+            //If number 2 empty
             } else if (num2 == null) {
+                //If operator empty
                 if (operator == null) {
-                    num1 = parseInt(num1.toString() + string);
-                    screencontent.textContent += string;
-                } else {
-                    num2 = parseInt(string);
+                    if (string == ".") {
+                         if (!num1HasDec) {
+                            num1 = num1 + string;
+                            screencontent.textContent += string;
+                            num1HasDec = true;
+                         }
+                    } else {
+                        num1 = num1 + string;
+                        screencontent.textContent += string;
+                    }
+                //If has operator
+                } else if (string != ".") {
+                    num2 = string;
                     screencontent.textContent += (" " + string);
                 }
+            //If everything full
             } else {
-                num2 = parseInt(num2.toString() + string);
-                screencontent.textContent += string;
+                if (string == ".") {
+                    if (!num2HasDec) {
+                        num2 = num2 + string;
+                        screencontent.textContent += string;
+                        num2HasDec == true;
+                    }
+                } else {
+                    num2 = num2 + string;
+                    screencontent.textContent += string;
+                }
             }
+        //If operator
         } else {
+            //If number 1 full
             if (num1 != null) {
+                //If number 2 empty
                 if (num2 == null) {
                     operator = string;
                     screencontent.textContent += (" " + string);
+                //If number 2 full
                 } else {
-                    if (isNaN(parseInt(operate(num1, num2, operator)))) {
-                        clearDisplay(); screencontent.textContent = "Don't do that.";
+                    //Divide by zero check
+                    const ans = operate();
+                    if (ans == "Don't do that.") {
+                        clearDisplay();
+                        screencontent.textContent = "Don't do that.";
                     } else {
-                        num1 = parseInt(operate(num1, num2, operator));
+                        if (ans.includes(".")) {num1HasDec = true;} else {num1HasDec = false;}
+                        num1 = ans;
                         operator = string;
                         screencontent.textContent = num1 + " " + operator;
                     }
@@ -79,6 +132,8 @@ const addToDisplay = function(string) {
     console.log("num1: " + num1);
     console.log("num2: " + num2);
     console.log("operator: " + operator);
+    console.log("num1HasDec: " + num1HasDec);
+    console.log("num2HasDec: " + num2HasDec);
 }
 
 const btn1 = document.querySelector("#one");
@@ -91,6 +146,7 @@ const btn7 = document.querySelector("#seven");
 const btn8 = document.querySelector("#eight");
 const btn9 = document.querySelector("#nine");
 const btn0 = document.querySelector("#zero");
+const btndec = document.querySelector('#dec');
 const btnclear = document.querySelector("#clear");
 const btneq = document.querySelector("#eq");
 const btnplus = document.querySelector("#plus");
@@ -108,9 +164,65 @@ btn7.addEventListener("click", () => {addToDisplay("7");})
 btn8.addEventListener("click", () => {addToDisplay("8");})
 btn9.addEventListener("click", () => {addToDisplay("9");})
 btn0.addEventListener("click", () => {addToDisplay("0");})
+btndec.addEventListener("click", () => {addToDisplay(".");})
 btnclear.addEventListener("click", () => {clearDisplay();})
-btneq.addEventListener("click", () => {if (num1 != null && num2 != null && operator != null) {addToDisplay(operate(num1, num2, operator));}})
+btneq.addEventListener("click", () => {if (num1 != null && num2 != null && operator != null) {addToDisplay(operate());}})
 btnplus.addEventListener("click", () => {addToDisplay("+");})
 btnsub.addEventListener("click", () => {addToDisplay("-");})
 btnmult.addEventListener("click", () => {addToDisplay("*");})
 btndiv.addEventListener("click", () => {addToDisplay("/");})
+
+const calculator = document.querySelector("body");
+
+calculator.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case "1":
+            addToDisplay("1");
+            break;
+        case "2":
+            addToDisplay("2");
+            break;
+        case "3":
+            addToDisplay("3");
+            break;
+        case "4":
+            addToDisplay("4");
+            break;
+        case "5":
+            addToDisplay("5");
+            break;
+        case "6":
+            addToDisplay("6");
+            break;
+        case "7":
+            addToDisplay("7");
+            break;
+        case "8":
+            addToDisplay("8");
+            break;
+        case "9":
+            addToDisplay("9");
+            break;
+        case "0":
+            addToDisplay("0");
+            break;
+        case ".":
+            addToDisplay(".");
+            break;
+        case "Enter":
+            if (num1 != null && num2 != null && operator != null) {addToDisplay(operate());}
+            break;
+        case "+":
+            addToDisplay("+");
+            break;
+        case "-":
+            addToDisplay("-");
+            break;
+        case "*":
+            addToDisplay("*");
+            break;
+        case "/":
+            addToDisplay("/");
+            break;
+    }
+})
